feb95d7921b1af1b2aa3f335dea145376a7cfc7c
SimonSisay <Simonsisay9@gmail.com>
2019-11-14 16:02:45 +0300

    (feat)/finished logic: the form now generator and validates any given valid form schema "

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import FormGenerator from "./FormGenerator";
+import { formSchema } from "./testFormSchema";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const FormGeneratorWrapper = ({ children }) => {
+  let defaultValues = {};
+  formSchema.forEach(field => {
+    if (field.defaultValue) {
+      defaultValues = { ...defaultValues, [field.name]: field.defaultValue };
+    }
+  });
+  return <React.Fragment>{children(formSchema, defaultValues)}</React.Fragment>;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const submitFormAsync = data => {
+  console.log(data);
+};
+
+ReactDOM.render(
+  <FormGeneratorWrapper>
+    {(formSchema, defaultValues) => (
+      <FormGenerator
+        formSchema={formSchema}
+        defaultValues={defaultValues}
+        submitFormAsync={submitFormAsync}
+        renderSubmitButton={handleSubmit => (
+          <button type="submit" onClick={() => handleSubmit()}>
+            Submit
+          </button>
+        )}
+      />
+    )}
+  </FormGeneratorWrapper>,
+  document.getElementById("root")
+);
